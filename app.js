45270a782144d23b93ca21f9ce4b7e9c12c12767
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-06-06 17:18:45 +0200

    feat: middlewares

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const postRouter = require("./routers/posts.js");
+const errorHandler = require("./middlewares/errorHandler.js");
+const notFound = require("./middlewares/notFound.js");
 const app = express();
 
 require("dotenv").config();
@@ -8,6 +10,8 @@ const port = PORT || 3000;
 
 app.use(express.json());
 app.use("/posts", postRouter);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server attivo su http://localhost:${port}`);
